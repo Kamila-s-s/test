2ffcce13cf9231cd8a1b82cc256b28523d65f431
Ненартович Алексей Антонович <Nenartovich@WUD.lan>
2019-10-03 13:55:01 +0300

    Add 2

--- Hello.java
+++ Hello.java
@@ -1,14 +1,10 @@
 public class Hello{
 
 public static void main(String[] args){
-<<<<<<< HEAD
-System.out.println("Hello!!!");
+
+System.out.println("Hello!!!2");
 System.out.println("B!!!");
 
-=======
-    System.out.println("A!!!");
-System.out.println("Hello!!!");
->>>>>>> 6265546f2720ba86e4c521dc44a67f9964786ed3
 
 }
 }
